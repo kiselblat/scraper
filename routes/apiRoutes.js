2fe4724dab55960014fa2f890db2f935afcd2548
Tom <tsechrist@outlook.com>
2019-08-20 15:48:00 -0500

    Moves required dependencies to apiRoutes so scrape works Also slims down the server a little

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,9 @@
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
 var db = require("../models");
 
 module.exports = (app) => {
--- server.js
+++ server.js
@@ -1,44 +1,29 @@
+// Import dependencies
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
-
 // Require all models
 var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater";
 
 // Initialize Express
 var app = express();
 
 // Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater";
 
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 // Routes
